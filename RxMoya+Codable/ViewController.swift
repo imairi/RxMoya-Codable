281845949cc7f9828df98f18092af20a1230862d
imairi <imairi@gmail.com>
2017-09-10 21:51:33 +0900

    Initial Commit

---
+++ RxMoya+Codable/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RxMoya+Codable
+//
+//  Created by Imairi Yosuke on 2017/09/10.
+//  Copyright © 2017年 Imairi Yosuke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
